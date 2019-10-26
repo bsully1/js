8319ac245e58e8912232e60af5d4cd0861651f68
Brandon Sullivan <jbs81890@gmail.com>
2019-10-26 19:19:16 +0000

    lambda practice

---
+++ sort.rb
@@ -0,0 +1,36 @@
+class Person
+    attr_reader :age, :name
+    def initialize(name, age)
+      @name = name
+      @age = age
+    end
+
+end
+
+brandon = Person.new("Brandon", 29)
+ken = Person.new("Ken", 28)
+eric = Person.new("Eric", 29)
+cindy = Person.new("Cindy", 14)
+
+people = [brandon, ken, eric, cindy]
+
+def sort_people(people, comparer)
+  people.sort do |x, y|
+    x.name <=> y.name
+     comparer.call(x) <=> comparer.call(y)
+  end
+end
+
+grab_age = lambda do |person|
+    person.age
+end
+
+grab_name = lambda do |person|
+  person.name
+end
+
+
+sorted_people = sort_people(people, grab_name)
+
+puts sorted_people.inspect
+
